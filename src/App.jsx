760fa11d3ac0af9999655b573b5dfc5bc431d1b9
kabindrr <chaina_kabindra@yahoo.com>
2024-06-21 09:58:23 +1000

    initialization of the app

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      Hello from developer entry point
+      <h1>Welcome to count your beer</h1>
+    </>
+  );
+}
+
+export default App;
